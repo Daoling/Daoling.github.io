da313da347593a434510cae2b8608d9a40b29848
Daoling Song <faustpeace@gmail.com>
2014-12-08 00:49:20 +0800

    try

--- js/sdl_todo.js
+++ js/sdl_todo.js
@@ -1,1 +1,7 @@
+(fuction(){
+var Angular_content = { name: 'First', content: 11};
 var app = angular.module('sdl_App', []);
+app.controller('Blog',function(){
+    this.Angular = Angular_content;
+});
+})();

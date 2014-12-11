6c883042ae4891bacd961f37be355af105e8ac06
Daoling Song <faustpeace@gmail.com>
2014-12-12 01:38:55 +0800

    add some css

--- js/sdl_todo.js
+++ js/sdl_todo.js
@@ -4,7 +4,7 @@ var app = angular.module('sdl_App', [])
 app.controller('Angular', function($scope) {
   $scope.Articles = [{
   name:'Closure',
-  content:' the function defined in the closure remembers the environment in which it was created.'
+  content:'the function defined in the closure remembers the environment in which it was created.'
   },{
   name:'Use Strict',
   content:'During the javascript, use strict means: use a variable without declaring it, is not allowed.'

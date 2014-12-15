1ad46f7607daea59530b30d3180f63512da4abeb
Daoling Song <faustpeace@gmail.com>
2014-12-16 00:49:48 +0800

    Logistic Regression

--- js/sdl_todo.js
+++ js/sdl_todo.js
@@ -4,7 +4,7 @@ var app = angular.module('sdl_App', [])
 app.controller('Angular', function($scope) {
   $scope.Articles = [{
   name:'Closure',
-  content:'• the function defined in the closure remembers the environment in which it was created. See more: http://marcofranssen.nl/writing-modular-javascript-without-polluting-the-global-namespace/'
+  content:'<p>• the function defined in the closure remembers the environment in which it was created. See more: http://marcofranssen.nl/writing-modular-javascript-without-polluting-the-global-namespace/</p>'
   },{
   name:'Use Strict',
   content:'• During the javascript, use strict means: use a variable without declaring it, is not allowed. See more http://www.w3schools.com/js/js_strict.asp.'
@@ -49,13 +49,22 @@ app.controller('Angular', function($scope) {
   content:'• Use Feature Scaling, make gradient descent run much faster and converge in a lot few other iterations. It means get every feature into approximately a -1<x<1 range.'
   },{
   name:'Trick to make gradient descent work well.(2)',
-  content:'learning rate alpha. If α is too small: slow convergence. If α is too large : J(θ) may not decrease on every iteration; may not converge.To choose  α, try …, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1….'
+  content:'• learning rate alpha. If α is too small: slow convergence. If α is too large : J(θ) may not decrease on every iteration; may not converge.To choose  α, try …, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1….'
   },{
   name:'Normal Equation',
-  content:'θ = (XTX)-1XTY.'
+  content:'• θ = (XTX)-1XTY.'
   },{
   name:'When to use Gradient Descent, or Normal Equation',
-  content:'Gradient Descent: 1.Need to choose α. 2.Needs many iterations. 3.Work well even when n is large.    Normal Equation: 1.No need to choose α.  2.Dont’t need to iterate.  3.Need to compute  (XTX)-1 (sometimes it is no answer)    4.Slow if n is very large. (n>10000)'
+  content:'• Gradient Descent: 1.Need to choose α. 2.Needs many iterations. 3.Work well even when n is large.    Normal Equation: 1.No need to choose α.  2.Dont’t need to iterate.  3.Need to compute  (XTX)-1 (sometimes it is no answer)    4.Slow if n is very large. (n>10000)'
+  },{
+  name:'Logistic Regression',
+  content:'• Logistic Regression also called Classification Rrgression. 0≤hθ(x)≤1'
+  },{
+  name:'Logistic Regression',
+  content:'• hθ(x)=g(θTx), g(z)=1/(1+e^(-z)).  '
+  },{
+  name:'Sigmoid function',
+  content:'• g(z)=1/(1+e^(-z)). '
   },{
   name:'',
   content:''
